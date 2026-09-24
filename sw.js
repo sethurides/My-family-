791cc5f5f0313a74da4ee853e7c824bff7106246
sethurides <devivantapati1@gmail.com>
2026-09-24 20:43:35 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,169 @@
-const CACHE_NAME = "my-money-v1";
+const CACHE_NAME = "my-money-v3";
 
-const FILES = [
+const APP_FILES = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./my-qr.png"
 ];
+
+
+/* ===============================
+   INSTALL
+   =============================== */
 
 self.addEventListener(
   "install",
-  event => {
+  function (event) {
 
     event.waitUntil(
 
-      caches.open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(FILES)
-        )
+      caches
+        .open(CACHE_NAME)
+        .then(function (cache) {
+
+          return cache.addAll(
+            APP_FILES
+          );
+
+        })
 
     );
+
+    self.skipWaiting();
 
   }
 );
 
 
+/* ===============================
+   ACTIVATE
+   =============================== */
+
+self.addEventListener(
+  "activate",
+  function (event) {
+
+    event.waitUntil(
+
+      caches.keys()
+        .then(function (cacheNames) {
+
+          return Promise.all(
+
+            cacheNames
+              .filter(function (cacheName) {
+
+                return (
+                  cacheName !==
+                  CACHE_NAME
+                );
+
+              })
+              .map(function (cacheName) {
+
+                return caches.delete(
+                  cacheName
+                );
+
+              })
+
+          );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+/* ===============================
+   FETCH
+   =============================== */
+
 self.addEventListener(
   "fetch",
-  event => {
+  function (event) {
+
+    if (
+      event.request.method !==
+      "GET"
+    ) {
+
+      return;
+
+    }
+
 
     event.respondWith(
 
-      caches.match(event.request)
-        .then(response => {
+      caches.match(
+        event.request
+      )
+      .then(function (cachedResponse) {
 
-          return response ||
-            fetch(event.request);
+        if (cachedResponse) {
+
+          return cachedResponse;
+
+        }
+
+
+        return fetch(
+          event.request
+        )
+        .then(function (networkResponse) {
+
+          /*
+             Save only successful
+             same-origin responses.
+          */
+
+          if (
+            networkResponse &&
+            networkResponse.status === 200 &&
+            networkResponse.type ===
+              "basic"
+          ) {
+
+            const responseClone =
+              networkResponse.clone();
+
+
+            caches.open(
+              CACHE_NAME
+            )
+            .then(function (cache) {
+
+              cache.put(
+                event.request,
+                responseClone
+              );
+
+            });
+
+          }
+
+
+          return networkResponse;
 
         })
+        .catch(function () {
+
+          return caches.match(
+            "./index.html"
+          );
+
+        });
+
+      })
 
     );
 
