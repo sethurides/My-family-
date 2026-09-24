9e55341777e6c3c2b61d0b2a5a1e432afdf6ddd2
sethurides <devivantapati1@gmail.com>
2026-09-24 20:18:57 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -3,29 +3,43 @@ const CACHE_NAME = "my-money-v1";
 const FILES = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./sw.js",
-  "./qr.png",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener(
+  "install",
+  event => {
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-  );
+    event.waitUntil(
 
-});
+      caches.open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FILES)
+        )
 
-self.addEventListener("fetch", event => {
+    );
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  }
+);
 
-});
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      caches.match(event.request)
+        .then(response => {
+
+          return response ||
+            fetch(event.request);
+
+        })
+
+    );
+
+  }
+);
